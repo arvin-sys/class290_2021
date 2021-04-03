9d38a8ae06f637a70ca5a1459fbe7a4bb23958d4
arvin-sys <57642837+arvin-sys@users.noreply.github.com>
2021-04-03 16:50:26 +0200

    Making admin updates

--- express_example/src/auth/auth.service.js
+++ express_example/src/auth/auth.service.js
@@ -2,24 +2,35 @@ const User = require('../users/user.entity');
 const { Unauthorized } = require('http-errors')
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const { Locked } = require('http-errors');
 
 class AuthService {
+
     async validate(username, password) {
         const user = await User.findOne({ username });
-        if (!user || !bcrypt.compareSync(password, user.password)) {
-            throw new Unauthorized();
-        }
 
+        if (!user) {
+            throw new Error('user not found');
+        }
+        if(user.isLocked){
+            throw new Locked('The user is locked!')
+        }
+        if(user.loginAttempts === 3){
+            await User.findOneAndUpdate({'username' : username}, {'isLocked' :true})
+            throw new Locked('The user is locked!')
+        }
+        if(!bcrypt.compareSync(password, user.password)){
+           await User.findOneAndUpdate( {'username' : username}, {$inc:{ 'loginAttempts' : 1}})
+           throw new Error('wrong password, attemp count:'+ user.loginAttempts)
+        }
         return user;
     }
-
+    
     async login(username, password) {
         const user = await this.validate(username, password);
-
         const token = jwt.sign({ userId: user._id, username: user.username }, process.env.JWT_SECRET, {
             expiresIn: process.env.JWT_EXPIRES_IN
         })
-
         return token;
     }
 
@@ -27,7 +38,6 @@ class AuthService {
         const obj = jwt.verify(token, process.env.JWT_SECRET, {
             ignoreExpiration: false
         })
-
         return { userId: obj.userId, username: obj.username };
     }
 }
